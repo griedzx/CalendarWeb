2d73c17b4a18d18f8e411626ca69058c41c27bcf
griedzx <2719323380@qq.com>
2024-04-20 01:02:32 +0800

    1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
   var topnav = document.querySelector('.topnav');
-  var scrollPosition = window.pageYOffset;
+  var scrollPosition = window.scrollY;
   var maxOpacityChangeDistance = 500; 
 
   var opacity = 0.1 + (scrollPosition / maxOpacityChangeDistance) * (1 - 0.1);
@@ -9,22 +9,22 @@ window.addEventListener('scroll', function() {
   topnav.style.backgroundColor = 'rgba(225, 225, 225, ' + opacity + ')';
 });
 
-var blogEvents;  // 定义一个全局变量来保存博客事件
+var blogEvents;  
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
-
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth'
+    initialView: 'dayGridMonth',
+    eventClick: function(info) {
+      window.open(info.event.url, '_blank');
+      info.jsEvent.preventDefault();
+    }
   });
-
   calendar.render();
 
-  // 使用 fetch API 从博客获取文章数据
   fetch('https://griedzx.github.io/index.json')
     .then(response => response.json())
     .then(articles => {
-      // 创建一个新的数组，该数组只包含每个标题的最新文章
       const events = articles.reduce((acc, current) => {
         const x = acc.find(item => item.title === current.title);
         if (!x) {
@@ -35,35 +35,14 @@ document.addEventListener('DOMContentLoaded', function() {
       }, []).map(article => ({
         title: article.title,
         start: article.date,
-        url: article.RelPermalink,  // 修改这一行
+        url: article.uri,
       }));
 
-      // 将转换后的数据添加到日历的 events 中
       calendar.addEventSource(events);
       calendar.render();
 
-      // 保存博客事件到全局变量中
       blogEvents = events;
     })
     .catch(error => console.error('Error:', error));
 });
 
-document.getElementById('blogEventsCheckbox').addEventListener('change', function() {
-  var calendar = new FullCalendar.Calendar(document.getElementById('calendar'), {
-    initialView: 'dayGridMonth'
-  });
-
-  if (this.checked) {
-    // 如果复选框被选中，添加博客文章事件
-    calendar.addEventSource(blogEvents);
-    alert('Checkbox is checked. Blog events are added.');
-  } else {
-    // 如果复选框没有被选中，移除博客文章事件
-    calendar.getEvents().forEach(function(event) {
-      if (event.source === blogEvents) {
-        event.remove();
-      }
-    });
-    alert('Checkbox is not checked. Blog events are removed.');
-  }
-});
--- php/login.php
+++ php/login.php
@@ -1,12 +1,69 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "web";
 
-    class Login{
-        private $link;
-        public function_construct{
-            error_reporting(0);
-            session_start();
+$conn = new mysqli($servername, $username, $password);
 
-            #connect to server
-            
-        }
-    }
+if ($conn->connect_error) {
+  die("连接失败: " . $conn->connect_error);
+}
+
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sql) === TRUE) {
+  echo "数据库创建成功";
+} else {
+  echo "创建数据库错误: " . $conn->error;
+}
+
+$conn->select_db($dbname);
+
+$sql = "CREATE TABLE IF NOT EXISTS Users (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+password VARCHAR(30) NOT NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Users表存在";
+} else {
+  echo "创建用户表错误: " . $conn->error;
+}
+
+//插入测试账户和密码
+$sql = "SELECT * FROM Users WHERE username = 'test'";
+$result = $conn->query($sql);
+
+if ($result->num_rows == 0) {
+  // 如果没有找到测试账户，那么插入新的测试账户
+  $sql = "INSERT INTO Users (username, password) VALUES ('test', '123456')";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "测试账户创建成功";
+  } else {
+    echo "创建测试账户错误: " . $conn->error;
+  }
+}
+
+$post_username = $_POST['username'];
+$post_password = $_POST['password'];
+
+// 从数据库中检索用户的账号和密码
+$sql = "SELECT password FROM Users WHERE username = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $post_username);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+
+
+if ($row && $row['password'] == $post_password) {
+  echo "登录成功";
+  header("Location: /CalendarWeb/calendar.html");
+} else {
+  echo "登录失败";
+}
+
+$conn->close();
+?>
